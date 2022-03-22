216f6bef673b2d97762cb1e44d181ab1eb0f6740
RodrigoMartinezVega <rodrigomartinez123@aragon.unam.mx>
2022-03-22 12:39:04 -0600

    Clase 22/03/2022

--- clases/ReutilizacionCodigo2209/src/reutilizacioncodigo2209/ReutilizacionCodigo2209.java
+++ clases/ReutilizacionCodigo2209/src/reutilizacioncodigo2209/ReutilizacionCodigo2209.java
@@ -10,6 +10,9 @@ import ico.fes.componentes.Mouse;
 import ico.fes.componentes.Procesador;
 import ico.fes.componentes.Teclado;
 import ico.fes.herencia.Alumno;
+import ico.fes.herencia.Servidor;
+import ico.fes.herencia.polimorfismo.Animal;
+import ico.fes.herencia.polimorfismo.Perro;
 import javax.swing.JFrame;
 
 /**
@@ -32,29 +35,47 @@ public class ReutilizacionCodigo2209 {
         ventana.setSize(600, 400);
         ventana.setVisible(true);
         */
-        Computadora miCompu= new Computadora();
+         Computadora miCompu= new Computadora();
         miCompu.setMarca("Asus");
         miCompu.setCpu(new Procesador("Intel", 2.6f));
-        System.out.println( miCompu );
+        System.out.println(miCompu);
         miCompu.getCpu().setMarca("AMD");
         System.out.println(miCompu);
-        //Ejercicio: Establecer un mouse marca Logitch del tipo óptico
+        //ejercicio establecer mouse marca logitech tipo optico
         miCompu.setRaton(new Mouse("Logitech", "Óptico"));
         System.out.println(miCompu);
         
-        Computadora compu2 = new Computadora("Apple", "MacBook Pro",new Monitor("Toshiba",14.3f) , new Mouse("Acteck","óptico"), new Teclado("Apple",101), new Procesador("M1", 3.4f));
-        System.out.println(compu2);
-        compu2.getRaton().setMarca("Apple");
-        compu2.getRaton().setTipo("Touch");
-        System.out.println(compu2);
+        Computadora compu2 = new Computadora("Apple", "MacBook Pro", 
+                new Monitor("toshiba",14),
+                new Mouse("Acteck", "optico"),
+                new Teclado("Apple", 101),
+                new Procesador("M1", 2.4f));
         
-        System.out.println("------------------------------");
-        Alumno alu1=new Alumno();
-        alu1.setNombre("José");
-        System.out.println(alu1);
-        
-        Alumno alu2 = new Alumno("1243243244","ICO, José Perez",19);
-        System.out.println(alu2);
+         compu2.setRaton(new Mouse("apple", "Tocuh"));
+         System.out.println(compu2);
+         
+         System.out.println("----------------------------------------");
+         Alumno alu1=new Alumno();
+         alu1.setNombre("Jose");
+         alu1.setEdad(13);
+         
+         Alumno alu2 = new Alumno("319097944", "ico", "jose", 20);
+         System.out.println(alu2);
+         
+         System.out.println("-----------composicion y herencia en la misma clase--------------------------------");
+         
+         Servidor server = new Servidor();
+         server.setNumeroTarjetasRed(2);
+         server.setMarca("HP");
+         server.getRaton().setMarca("Logitech");
+         server.getRaton().setTipo("optico");
+         
+         System.out.println("------------ejemplo polimorfismo------------");
+         Perro dog = new Perro("bull dog", "5 estrellas", 4);
+         Animal animal = new Animal(4);
+         dog.emitirSonido();
+         animal.emitirSonido();
+       
     }  
     
 }
